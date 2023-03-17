1d76121c08386f57f51aed2504e0d8d2b0a89f47
hphuss <125681119+hphuss@users.noreply.github.com>
2023-03-17 22:17:03 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 import streamlit as st
-c=st.write("GIẢI PHƯƠNG TRÌNH BẬC NHẤT")
-a = st.number_input('Tham số a')
-b = st.number_input('Tham số b')
-
-if st.button('Giải'):
-  if a != 0:
-    st.write("Phương trình có 1 nghiệm", (-b)/a)
-  else:
-    st.write("Phương trình vô nghiệm")
+import pickle
+import numpy as np
+model=pickle.load(open('model.pickle','rb'))
+st.title("Revenue Prediction")
+x=st.number_input('Input Temperature')
+if st.button('Predict'):
+  x=np.array(x).reshape(-1,1)
+  result=model.predict(x)
+  st.caption('Revenue Prediction')
+  ket_qua=result[0]
+  ket_qua=str(ket_qua)
+  st.success(ket_qua)
